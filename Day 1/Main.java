d7934edb826b4abeba39b4713d17d60ca24c2b6f
Hrishabh7405 <hrishabh07042005@gmail.com>
2026-01-05 23:32:51 +0530

    Day 1: Practiced basic variables and types

--- Day 1/Main.java
+++ Day 1/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Day 1 has started. Java is working.");
+        
+        int age = 25;
+        double marks = 92.5;
+        boolean isPassed = true;
+        String name = "Alex";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Passed: " + isPassed);
     }
 }
