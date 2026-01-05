6f25bf1b7cd2dcd656338f960584b130a3cc5bed
Hrishabh7405 <hrishabh07042005@gmail.com>
2026-01-05 23:47:56 +0530

    Practiced loops (for and while)

--- Day 1/Main.java
+++ Day 1/Main.java
@@ -1,16 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         
-        int marks = 40;
-
-        if (marks >= 90) {
-            System.out.println("Grade: A");
-        } else if (marks >= 75) {
-            System.out.println("Grade: B");
-        } else if (marks >= 60) {
-            System.out.println("Grade: C");
-        } else {
-            System.out.println("Grade: Fail");
+        int i = 1;
+        while (i <=5) {
+            System.out.println("Count: " + i);
+            i++;
         }
     }
 }
