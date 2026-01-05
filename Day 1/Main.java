08dead9a6958d5d0143a12d8706716e125038678
Hrishabh7405 <hrishabh07042005@gmail.com>
2026-01-06 00:12:39 +0530

    Combined arrays, loops, methods, and conditions

--- Day 1/Main.java
+++ Day 1/Main.java
@@ -2,7 +2,15 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {2, 5, 8, 11, 14};
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+            String oddEven = oddEven(numbers[i]);
+            System.out.println("Number " + numbers[i] + " is " + oddEven);
+        }
+    }
+    public static String oddEven(int number){
+        if (number % 2 == 0) {
+            return "even.";
+        } else {
+            return "odd.";
         }
     }
 }
