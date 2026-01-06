0fa61d19cd21ce2d7cd679214adedcf7de36daf4
Hrishabh7405 <hrishabh07042005@gmail.com>
2026-01-06 17:02:56 +0530

    Stored students in a collection and iterated

--- Day 2/StudentApp.java
+++ Day 2/StudentApp.java
@@ -1,18 +1,25 @@
+import java.util.ArrayList;
 public class StudentApp {
     public static void main(String[] args) {
         System.out.println("Student Record Manager started");
 
-    Student student1 = new Student();
-    student1.id = 1;
-    student1.name = "Alice";
-    student1.marks = 88;
+        ArrayList<Student> students = new ArrayList<>();
 
-    Student student2 = new Student();
-    student2.id = 2;
-    student2.name = "Bob";
-    student2.marks = 74;
+        Student student1 = new Student();
+        student1.id = 1;
+        student1.name = "Alice";
+        student1.marks = 88;
 
-    System.out.println(student1.name + " scored " + student1.marks);
-    System.out.println(student2.name + " scored " + student2.marks);
+        Student student2 = new Student();
+        student2.id = 2;
+        student2.name = "Bob";
+        student2.marks = 74;
+
+        students.add(student1);
+        students.add(student2);
+
+        for (Student s : students) {
+            System.out.println(s.name + " scored " + s.marks);
+        }
     }
 }
