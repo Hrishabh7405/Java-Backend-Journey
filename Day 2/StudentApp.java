f1aecb0d9f3d604a048940a093949166981f27c6
Hrishabh7405 <hrishabh07042005@gmail.com>
2026-01-06 16:55:59 +0530

    linked Student model with application

--- Day 2/StudentApp.java
+++ Day 2/StudentApp.java
@@ -1,5 +1,18 @@
 public class StudentApp {
     public static void main(String[] args) {
         System.out.println("Student Record Manager started");
+
+    Student student1 = new Student();
+    student1.id = 1;
+    student1.name = "Alice";
+    student1.marks = 88;
+
+    Student student2 = new Student();
+    student2.id = 2;
+    student2.name = "Bob";
+    student2.marks = 74;
+
+    System.out.println(student1.name + " scored " + student1.marks);
+    System.out.println(student2.name + " scored " + student2.marks);
     }
 }
