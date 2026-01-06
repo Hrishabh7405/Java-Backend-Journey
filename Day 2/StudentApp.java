0e8139d8bef8e73487f0c78a5c484587fb70cce2
Hrishabh7405 <hrishabh07042005@gmail.com>
2026-01-06 17:08:10 +0530

    introduced StudentManager for business logic

--- Day 2/StudentApp.java
+++ Day 2/StudentApp.java
@@ -1,25 +1,7 @@
 import java.util.ArrayList;
 public class StudentApp {
     public static void main(String[] args) {
-        System.out.println("Student Record Manager started");
-
-        ArrayList<Student> students = new ArrayList<>();
-
-        Student student1 = new Student();
-        student1.id = 1;
-        student1.name = "Alice";
-        student1.marks = 88;
-
-        Student student2 = new Student();
-        student2.id = 2;
-        student2.name = "Bob";
-        student2.marks = 74;
-
-        students.add(student1);
-        students.add(student2);
-
-        for (Student s : students) {
-            System.out.println(s.name + " scored " + s.marks);
-        }
+        StudentManager manager = new StudentManager();
+        System.out.println("Student Manager initialized.");
     }
 }
---
+++ Day 2/StudentManager.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class StudentManager {
+    ArrayList<Student> students = new ArrayList<>();
+    
+}
